f0c8b35428872c8e5d1d8e29a414bd1426d8d453
MdeMora <miguel.uni.demora@gmail.com>
2022-10-04 20:56:17 +0200

    added emotion and removed boilerplate code

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <Box>
+      Welcome to <a href="https://nextjs.org">Next.js!</a>
+    </Box>
+  );
+};
+
+export default Home;
---
+++ src/theme/index.ts
@@ -0,0 +1,20 @@
+import { createTheme } from "@mui/material/styles";
+import { red } from "@mui/material/colors";
+
+// Create a theme instance.
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#775b59",
+      dark: "#4a3130",
+      light: "#a68886",
+    },
+    secondary: {
+      main: "#a8f9ff",
+      dark: "#75c6cc",
+      light: "#dcffff",
+    },
+  },
+});
+
+export default theme;
